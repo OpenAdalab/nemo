20d2499c2e781f7fa4864cd2c5bc729848d9a2f7
Lucile <lucile.brun@gmx.com>
2026-01-27 13:31:27 +0100

    [ENH] add atlases option to XCP-D

--- rsfmri/run_xcpd.py
+++ rsfmri/run_xcpd.py
@@ -154,6 +154,7 @@ def generate_slurm_script(config, subject, session, path_to_script, job_ids=None
         f'      --mode linc \\\n'
         f'      --bids-filter-file /bids_filter_dir/bids_filter_{session}.json \\\n'
         f'      --nuisance-regressors 36P \\\n'
+        f'      --atlases {" ".join(xcpd["atlases"])} \\\n'
         f'      --work-dir /out/work \\\n'
         f'      --config-file /config/xcpd_config.toml \\\n'
     )
